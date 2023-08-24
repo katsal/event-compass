f2957dce51e6d5c659a87b73b5ec04d45502511b
Caitlyn Khoo <138352657+CaitlynKhoo@users.noreply.github.com>
2023-08-24 12:24:47 +1000

    Update list.rb
    Uncomment uniqueness

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   has_many :event_lists, dependent: :destroy
   has_many :events, through: :event_lists
   belongs_to :user
-  validates :name, presence: true#, uniqueness: { scope: :user_id }
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :user, presence: true
 end
