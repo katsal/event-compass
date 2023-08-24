5d9c3449431d3f9b480d1b05784ba43f1b8b3701
Kostas Piludis <kostas.piludis@gmail.com>
2023-08-24 23:11:10 +0900

    added map on show page and calendar button

---
+++ app/javascript/controllers/calendar_button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Use the add-to-calendar-button component -->
+    <add-to-calendar-button
+    name="Sample Event"
+    description="Play with me!"
+    startDate="2024-02-20"
+    startTime="10:15"
+    endTime="17:45"
+    timeZone="Asia/Tokyo"
+    location="World Wide Web"
+    options="'Apple','Google','Outlook.com'"
+    buttonStyle="round"
+    >
+</add-to-calendar-button>
+  </div>
+</template>
+
+<script>
+import 'add-to-calendar-button';
+
+export default {
+  name: 'YourComponentName',
+  // other component options...
+};
+</script>
