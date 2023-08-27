217b38fabaa1d968d0266c4493ff42416eab514c
Barry Roberts <barryjroberts@gmail.com>
2023-08-27 23:44:56 +0900

    minor fixes/changes

--- app/controllers/event_lists_controller.rb
+++ app/controllers/event_lists_controller.rb
@@ -3,11 +3,15 @@ class EventListsController < ApplicationController
   def create
     @event_list = EventList.new(event_list_params)
     authorize @event_list
-    if @event_list.save
-      flash[:notice] = "Event saved to list"
+    if EventList.exists?(event_id: @event_list.event_id, list_id: @event_list.list_id)
+      flash[:alert] = "Event is already in the list"
       redirect_to event_path(@event_list.event)
+    elsif @event_list.save
+      flash[:notice] = "Event saved to list"
+      redirect_to list_path(@event_list.list)
     else
       redirect_to event_path(@event_list.event)
+      flash[:notice] = "Event unable to be saved"
     end
   end
 
