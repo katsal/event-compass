cf6e6443d7fb6e42933801b0bef945ec492748cc
karthika sanal <karthikasanal20@gmail.com>
2023-08-28 17:51:45 +1000

    past events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize @user
+    user_lists = current_user.lists
+    @past_events = Event.joins(event_lists: :list)
+                        .where('lists.id IN (?) AND events.end_date < ?', user_lists.pluck(:id), Date.today)
+                        .order(end_date: :desc)
+                        .distinct
   end
 end
