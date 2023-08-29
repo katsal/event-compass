e7292d36854410a166ecc8dccef02692ed8af65f
Caitlyn Khoo <caitlynkhoo@gmail.com>
2023-08-29 15:44:04 +1000

    create consumer, this is how we listen into those channels)

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,7 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def subscribed
+      stream_for "messages_#{params[:user_id]}"
+    end
   end
 end
---
+++ app/javascript/controllers/messages_subscription_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="messages-subscription"
+export default class extends Controller {
+  connect() {
+  }
+}
