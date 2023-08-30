42cc3372491cf81d137cd37b97919b19adcc7d5b
karthika sanal <karthikasanal20@gmail.com>
2023-08-30 12:43:51 +1000

    Fixed photo upload. minor fix in comment card in user profile. Moved #follow and #unfollow  to outside #show as discussed with Kostas.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :update] do
     resources :messages, only: [:create, :index]
     member do
       post 'follow' => 'users#follow'
