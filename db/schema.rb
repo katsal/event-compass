a6e2d560e6d23e34638cc429f9d2487ac103bb10
karthika sanal <karthikasanal20@gmail.com>
2023-08-31 15:35:04 +1000

    Adding status to event_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_072329) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_052815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_072329) do
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["event_id"], name: "index_event_lists_on_event_id"
     t.index ["list_id"], name: "index_event_lists_on_list_id"
   end
