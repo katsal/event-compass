7708905cabb8a4fe8f841eb155ee28e157ad858d
Caitlyn Khoo <caitlynkhoo@gmail.com>
2023-08-31 11:04:25 +1000

    pull master bundle install and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -158,10 +158,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_072329) do
     t.string "name"
     t.string "location"
     t.text "introduction"
-    t.boolean "admin", default: false
     t.date "date_of_birth"
-
-
+    t.boolean "admin"
     t.text "favoritor_score"
     t.text "favoritor_total"
     t.string "status"
